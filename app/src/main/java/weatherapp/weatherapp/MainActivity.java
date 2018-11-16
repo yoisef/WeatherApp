28fb6f626a402fbc0667ad1025adf025b85f5aa4
yoisef <yosfessam2010@yahoo.com>
2018-11-16 21:40:04 +0200

    intialize models

--- app/src/main/java/weatherapp/weatherapp/MainActivity.java
+++ app/src/main/java/weatherapp/weatherapp/MainActivity.java
@@ -2,12 +2,32 @@ package weatherapp.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import weatherapp.weatherapp.models.CurWeather;
+import weatherapp.weatherapp.models.ForcWeather;
+import weatherapp.weatherapp.models.Forecastday;
+import weatherapp.weatherapp.models.Searchlist;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      
     }
 }
---
+++ app/src/main/java/weatherapp/weatherapp/models/CurWeather.java
@@ -0,0 +1,31 @@
+package weatherapp.weatherapp.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CurWeather {
+
+    @SerializedName("location")
+    @Expose
+    private Location location;
+    @SerializedName("current")
+    @Expose
+    private Current current;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Current getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Current current) {
+        this.current = current;
+    }
+
+}
